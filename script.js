561889c713b226708a94898188eabbbd355ff6db
Ibrahim Halit <104463962+ibo-ibo@users.noreply.github.com>
2022-06-16 01:25:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 
+// ## Array Cardio Day 2
+
+      const people = [
+        { name: "Wes", year: 1988 },
+        { name: "Kait", year: 1986 },
+        { name: "Irv", year: 1970 },
+        { name: "Lux", year: 2015 },
+      ];
+
+      const comments = [
+        { text: "Love this!", id: 523423 },
+        { text: "Super good", id: 823423 },
+        { text: "You are the best", id: 2039842 },
+        { text: "Ramen is my fav food ever", id: 123523 },
+        { text: "Nice Nice Nice!", id: 542328 },
+      ];
+
+      // Some and Every Checks
+
+      const today = new Date();
+      const presentYear = today.getFullYear();
+      const isAdult = people.some((person) => presentYear - person.year >= 19);
+      // Array.prototype.some() // is at least one person 19 or older?
+      console.log(isAdult);
+
+      // Array.prototype.every() // is everyone 19 or older?
+      const allAdults = people.every(
+        (person) => presentYear - person.year >= 19
+      );
+
+      console.log(allAdults);
+
+      // Array.prototype.find()
+      // Find is like filter, but instead returns just the one you are looking for
+      // find the comment with the ID of 823423
+      const theComment = comments.find((comment) => comment.id === 823423);
+      console.log(theComment);
+
+      // Array.prototype.findIndex()
+      // Find the comment with this ID
+      // delete the comment with the ID of 823423
+      const index = comments.indexOf(
+        comments.find((comment) => comment.id === 823423)
+      );
+      comments.splice(index, 1);
